f98282403c2f83cc3ccc701f176c31a1ac34fe73
JH <jhk3261@gnu.ac.kr>
2024-07-18 15:35:15 +0900

    Initial Commit

---
+++ DayLog/DayLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayLogApp.swift
+//  DayLog
+//
+//  Created by 감자의 맥북 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DayLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
